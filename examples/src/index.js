b989894357f76c1ad2fcff2adf35579d710bcaed
mjcampagna <mjcampagna@gmail.com>
2019-09-07 16:56:58 -0700

    Close modals by id, closeAllModals, open solo

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,8 +1,8 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { render } from 'react-dom'
 import { Modal, ModalContext, ModalProvider } from '../../src'
 
-const ModalOne = (props) => {
+const BasicModal = (props) => {
   const afterOpen = () => console.log('Fired after open.')
 	const beforeClose = () => console.log('Fired before close.')
 	const afterClose = () => console.log('Fired after close.')
@@ -23,10 +23,60 @@ const ModalOne = (props) => {
       onCancelLabel="Cancel"
       onSubmit={onSubmit}
       onSubmitLabel="Submit"
-      title="Standalone Modal"
+      title="Basic Modal w/ Callback Events"
+      {...props}
     >
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id ipsum ac eros sagittis tincidunt. Vivamus et placerat nisl. Cras viverra a velit ornare finibus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer egestas eros eu porta porta. Nullam elementum viverra risus ut pulvinar. Etiam porttitor scelerisque facilisis.</p>
-      <p>Curabitur finibus bibendum dapibus. Pellentesque non purus non mauris scelerisque tempor ac et lacus. Vestibulum porttitor mi sit amet condimentum ornare. Vestibulum vitae est augue. Sed placerat eleifend ex, nec ultrices mi mollis eu. Ut rutrum tincidunt odio, id dignissim dolor venenatis id. Vestibulum sit amet fringilla nisi. In eu risus fermentum tellus tincidunt viverra sit amet ac dolor. Aliquam sed felis imperdiet, dictum nibh in, cursus massa. Curabitur in elit ac erat porta pellentesque iaculis id tellus. Morbi pretium est elit, ut commodo tellus tincidunt quis.</p>
+      <p>This modal logs its callback events. Open the browser's console to view the events as they fire.</p>
+    </Modal>
+  )
+}
+
+const ChildModal = (props) => {
+  const { closeModal, closeAllModals } = useContext(ModalContext)
+  return (
+  <Modal
+    actions={[
+      <button onClick={() => closeModal(props.parent)} key="closeParent">Close Parent</button>,
+      <button onClick={closeAllModals} key="closeAll">Close All</button>,
+      'cancel',
+      'submit',
+    ]}
+    closeOnOutsideClick
+    title="Child Modal"
+    {...props}
+    >
+    <p>This modal is nested in a parent ({props.parent}).</p>
+  </Modal>
+)}
+
+const ParentModal = (props) => {
+  const { openModal, closeModal, closeAllModals } = useContext(ModalContext)
+  const afterOpen = () => console.log('Fired after open.')
+	const beforeClose = () => console.log('Fired before close.')
+	const afterClose = () => console.log('Fired after close.')
+	const onCancel = () => console.log('Modal canceled')
+	const onSubmit = () => console.log('Modal submitted')
+
+  return (
+    <Modal
+      actions={[
+        'cancel',
+        <button onClick={closeAllModals} key="closeAllModals">Close All</button>,
+        <button onClick={() => openModal(<ChildModal parent={props.id}/>)} key="ChildModal">Child Modal</button>,
+        'submit',
+      ]}
+      afterClose={afterClose}
+      afterOpen={afterOpen}
+      beforeClose={beforeClose}
+      closeOnOutsideClick
+      onCancel={onCancel}
+      onCancelLabel="Cancel"
+      onSubmit={onSubmit}
+      onSubmitLabel="Submit"
+      title="Parent Modal"
+      {...props}
+    >
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacus erat, hendrerit eget neque vel, scelerisque mattis dolor. Nunc tristique tortor nunc, non tincidunt massa imperdiet vitae. Praesent non orci urna. Proin eget nisl tellus. Praesent dignissim convallis maximus. Praesent lacinia tellus magna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nullam turpis augue, vehicula et arcu in, ullamcorper dapibus neque.</p>
     </Modal>
   )
 }
@@ -34,21 +84,35 @@ const ModalOne = (props) => {
 const View = () => {
   const { openModal } = useContext(ModalContext)
 
-	const handleClick = (e) => {
-    return openModal(<ModalOne />)
+	const handleBasicModal = (e) => {
+    return openModal(<BasicModal />)
+  }
+
+	const handleParentModal = (e) => {
+    return openModal(<ParentModal />)
   }
 
   return (
     <>
-      <button onClick={handleClick}>Open Modal #1</button>
+      <button onClick={handleBasicModal}>Basic Modal w/ Callback Events</button>
+      <button onClick={handleParentModal}>Parent Modal</button>
     </>
   )
 }
 
-const App = () => (
-  <ModalProvider>
-    <View />
-  </ModalProvider>
-)
+const App = () => {
+  const [solo, setSolo] = useState(false)
+  return (
+    <ModalProvider solo={solo}>
+      <div className="toggle-solo">
+        <label>
+          <input checked={!solo} type="checkbox" onChange={() => setSolo(!solo)} />
+          <span>Allow Modal Stacking</span>
+        </label>
+      </div>
+      <View />
+    </ModalProvider>
+  )
+}
 
 render(<App />, document.getElementById('root'))
--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const Modal = ({ children, className, ...props }) => {
     if (props.beforeClose) {
       props.beforeClose()
     }
-    closeModal()
+    closeModal(props.id)
     if (props.afterClose) {
       props.afterClose()
     }
@@ -65,7 +65,13 @@ const Modal = ({ children, className, ...props }) => {
     if (props.afterOpen) {
       props.afterOpen()
     }
-  }, [props.afterOpen])
+  }, [])
+
+  useEffect(() => {
+    if (props.enqueuedToClose.find(id => id === props.id)) {
+      handleClose()
+    }
+  }, [props.enqueuedToClose])
 
   if (!children) {
     return null
@@ -141,11 +147,24 @@ const Modal = ({ children, className, ...props }) => {
 
 const ModalContext = createContext()
 
-const ModalProvider = ({ children }) => {
+const ModalProvider = ({ children, solo = false }) => {
   const [modals, setModals] = useState([])
-  const closeModal = () => setModals([...modals.slice(0, -1)])
+  const [enqueuedToClose, setEnqueuedToClose] = useState([])
+  const closeModal = (id) => {
+    const index = modals.findIndex(modal => modal[0] === id)
+    if (index > -1) {
+      modals.splice(index, 1)
+    }
+    return setModals([...modals])
+  }
+  const closeAllModals = () => setEnqueuedToClose(modals.map(modal => modal[0]))
   const createRandomId = () => '_' + Math.random().toString(36).substr(2, 9)
-  const openModal = nextModal => setModals([...modals, [createRandomId(), nextModal]])
+  const openModal = nextModal => {
+    if (solo) {
+      closeAllModals()
+    }
+    setModals([...modals, [createRandomId(), nextModal]])
+  }
   const transitions = useTransition(modals, modal => modal[0], {
     from:  { opacity: 0 },
     enter: { opacity: 1 },
@@ -153,15 +172,21 @@ const ModalProvider = ({ children }) => {
   })
 
   return (
-    <ModalContext.Provider value={{ openModal, closeModal }}>
+    <ModalContext.Provider value={{ openModal, closeModal, closeAllModals }}>
       {children}
-      {transitions.map(({ item, key, props }) => (
-        <Portal id="modals" key={key}>
-          <animated.div style={props}>
-            {item[1]}
-          </animated.div>
-        </Portal>
-      ))}
+      {transitions.map(({ item, key, props }, index) => {
+        const modal = React.cloneElement(item[1], {
+          enqueuedToClose,
+          id: item[0],
+        })
+        return (
+          <Portal id="modals" key={key}>
+            <animated.div style={props}>
+              {modal}
+            </animated.div>
+          </Portal>
+        )
+      })}
     </ModalContext.Provider>
   )
 }
